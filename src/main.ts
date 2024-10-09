b7861db6e81dd21e1d001f6017022f4eb1ecc1fc
Liam Murray <liammurray404@gmail.com>
2024-10-09 14:57:04 -0700

    Step 2 Completed!

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Liam's Amazing Game";
 document.title = gameName;
 
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
@@ -14,4 +13,4 @@ app.append(header);
 const buttonText = "🚀";
 const button = document.createElement("button");
 button.innerHTML = buttonText;
-app.append(button)
+app.append(button);
