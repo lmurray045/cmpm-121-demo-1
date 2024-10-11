8f50c06291102bd3e58e80d270176a745c4cc6be
Liam Murray <liammurray404@gmail.com>
2024-10-10 21:44:48 -0700

    Step 2 should hopefully be done- it didn't commit properly last time

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Liam's Amazing Game";
+const gameName = "To the Moon!";
 document.title = gameName;
 
 const header = document.createElement("h1");
@@ -14,3 +14,16 @@ const buttonText = "🚀";
 const button = document.createElement("button");
 button.innerHTML = buttonText;
 app.append(button);
+
+//add a listener and a counter
+let counter: number = 0;
+const counterText: string = `GME Stock Price: ${counter}$`;
+const counterElement = document.createElement("div");
+counterElement.innerHTML = counterText;
+app.append(counterElement);
+
+button.addEventListener("click", () => {
+  counter += 1;
+  const updatedCounterText = `GME Stock Price: ${counter}$`;
+  counterElement.innerHTML = updatedCounterText;
+});
