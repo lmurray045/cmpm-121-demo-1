b631266f244e52bda18387a95989ab79d720b515
Liam Murray <liammurray404@gmail.com>
2024-10-13 15:36:59 -0700

    Step 9 Done!

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,86 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.style.position = "relative";
-header.style.bottom = "100px";
+header.style.bottom = "150px";
 header.innerHTML = gameName;
 app.append(header);
 
+//button generator
+class Button {
+  text:string ;
+  obj: HTMLButtonElement;
+  constructor(displayText: string, stylePosition: string, styleBottom: string, scale:string, styleLeft: string, styleRight?: string) {
+    //make the style object
+    this.text = displayText;
+    this.obj = document.createElement("button");
+    this.obj.innerHTML = this.text;
+
+    //style the button
+    this.obj.style.position = stylePosition;
+    this.obj.style.bottom = styleBottom;
+    this.obj.style.left = styleLeft;
+    this.obj.style.scale = scale;
+    if(styleRight) {
+      this.obj.style.right = styleRight;
+    }
+    app.append(this.obj)
+  }
+
+}
+
+//upgrade class
+class Upgrade {
+  text: string;
+  price: number;
+  scale: number;
+  button: Button;
+  timesBought: number;
+  hudElement: HTMLDivElement;
+
+  constructor(text: string, price: number, scale: number, stylePosition: string, styleBottom: string, styleLeft: string, styleRight: string, UIscale:string) {
+    //declare fields and make button
+    this.text = text;
+    this.price = price;
+    this.scale = scale;
+    this.timesBought = 0;
+    this.hudElement = document.createElement("div");
+
+    this.button = new Button(this.text + ` - ${price}$`, stylePosition, styleBottom, UIscale, styleLeft, styleRight);
+
+    //add listener to button
+    this.button.obj.addEventListener("click", () => {
+      //update numbers
+      this.timesBought += 1;
+      rate += this.scale / fps;
+      dps += this.scale;
+      counter -= this.price;
+      this.price *= 1.15;
+      //redisplay text
+      const updatedCounterText = this.text + ` - ${this.price.toFixed(2)}$`;
+      this.button.obj.innerHTML = updatedCounterText;
+    });
+  }
+  //run an update loop in the animation call
+  update() {
+    if (counter >= this.price) {
+      this.button.obj.disabled = false;
+    } else {
+      this.button.obj.disabled = true;
+    }
+    this.hudElement.innerHTML = `"${this.text}" purchased: ${this.timesBought}`;
+  }
+
+  makeHudElement(stylePosition: string, styleTop: string, styleLeft: string) {
+    this.hudElement.innerHTML = `"${this.text}" purchased: ${this.timesBought}`;
+    this.hudElement.style.position = stylePosition;
+    this.hudElement.style.top = styleTop;
+    this.hudElement.style.left = styleLeft;
+    app.append(this.hudElement);
+  }
+}
+
 //make a clickable button
-const buttonText = "🚀";
-const button = document.createElement("button");
-button.innerHTML = buttonText;
-button.style.position = "relative";
-button.style.bottom = "100px";
-button.style.scale = "1.4";
-app.append(button);
+const button = new Button("🚀", "relative", "150px", "0px", "1.4")
 
 //add a listener and a counter
 let counter: number = 0;
@@ -26,30 +94,54 @@ const counterText: string = `GME Stock Price: ${counter}$`;
 const counterElement = document.createElement("div");
 counterElement.innerHTML = counterText;
 counterElement.style.position = "relative";
-counterElement.style.bottom = "90px";
+counterElement.style.bottom = "140px";
 app.append(counterElement);
 
-button.addEventListener("click", () => {
+button.obj.addEventListener("click", () => {
   counter += 1;
   const updatedCounterText = `GME Stock Price: ${counter}$`;
   counterElement.innerHTML = updatedCounterText;
 });
 
-//declare rate related variables
+//make upgrades
+interface Item {
+  name: string,
+  cost: number,
+  rate: number
+};
 
+const availableItems : Item[] = [
+  {name: "Buy Stock", cost: 10, rate: 0.1},
+  {name: "Post on Reddit about your poor financial choices", cost: 100, rate: 2},
+  {name: "Withdraw your kids' college funds to invest", cost: 1000, rate: 50},
+];
+
+let formatTop: number = 300
+let formatHud: number = 350
+
+const upgradeList: Upgrade[] = []
+
+availableItems.forEach((item: Item) => {
+  const upg = new Upgrade(item.name, item.cost, item.rate, "absolute", `${formatTop}px`, "1", '20px', '20px')
+  upgradeList.push(upg)
+  upg.makeHudElement("absolute", `${formatHud}px`, "30px")
+  formatTop -= 50;
+  formatHud += 50;
+})
+
+//declare rate related variables
 let originTime: number = performance.now();
 let fps: number = 0;
 let rate: number = 0;
 let dps: number = 0;
 
 //hud text
-
 const dpsText: string = `"Profit" per second: ${dps.toFixed(2)}$`;
 const dpsElement = document.createElement("div");
 dpsElement.innerHTML = dpsText;
 dpsElement.style.position = "absolute";
-dpsElement.style.top = "60px";
-dpsElement.style.left = "60px";
+dpsElement.style.top = "30px";
+dpsElement.style.left = "30px";
 app.append(dpsElement);
 
 // Define the function to update the counter
@@ -60,6 +152,7 @@ function updateCounter() {
   fps = 1000 / (deltaTime - originTime);
   originTime = deltaTime;
   counter += rate;
+
   // Update the displayed text
   const updatedCounterText = `GME Stock Price: ${counter.toFixed(2)}$`;
   counterElement.innerHTML = updatedCounterText;
@@ -68,120 +161,15 @@ function updateCounter() {
   const updatedDpsText = `"Profit" per second: ${dps.toFixed(2)}$`;
   dpsElement.innerHTML = updatedDpsText;
 
-  //upgrade texts
-  const updatedU1Text = `Investments made: ${upgrade1.timesBought}`;
-  u1Element.innerHTML = updatedU1Text;
-
-  const updatedU2Text = `Posts created: ${upgrade2.timesBought}`;
-  u2Element.innerHTML = updatedU2Text;
-
-  const updatedU3Text = `Funds drained: ${upgrade3.timesBought}`;
-  u3Element.innerHTML = updatedU3Text;
-
   //make upgrades available
-  upgrade1.update();
-  upgrade2.update();
-  upgrade3.update();
+  upgradeList.forEach((upgrade) => {
+    upgrade.update()
+  })
 
   // Schedule the next update
   requestAnimationFrame(updateCounter);
 }
 
-//upgrade class
-
-class Upgrade {
-  text: string;
-  price: number;
-  scale: number;
-  upgrade: HTMLButtonElement;
-  timesBought: number;
-
-  constructor(text: string, price: number, scale: number) {
-    //declare fields and make button
-    this.text = text + ` - ${price}$`;
-    this.price = price;
-    this.scale = scale;
-    this.timesBought = 0;
-
-    this.upgrade = document.createElement("button");
-    this.upgrade.innerHTML = this.text;
-    this.upgrade.disabled = true;
-    app.append(this.upgrade);
-
-    //add listener to button
-    this.upgrade.addEventListener("click", () => {
-      //update numbers
-      this.timesBought += 1;
-      rate += this.scale / fps;
-      dps += this.scale;
-      counter -= this.price;
-      this.price *= 1.15;
-      //redisplay text
-      this.text = text + ` - ${this.price.toFixed(2)}$`;
-      const updatedCounterText = this.text;
-      this.upgrade.innerHTML = updatedCounterText;
-    });
-  }
-  //run an update loop in the animation call
-  update() {
-    if (counter >= this.price) {
-      this.upgrade.disabled = false;
-    } else {
-      this.upgrade.disabled = true;
-    }
-  }
-}
-
-const upgrade1 = new Upgrade("Buy Stock", 10, 0.1);
-const upgrade2 = new Upgrade(
-  "Post on Reddit about your poor financial choices",
-  100,
-  2,
-);
-const upgrade3 = new Upgrade(
-  "Withdraw your kids' college funds to invest",
-  1000,
-  50,
-);
-
-upgrade1.upgrade.style.position = "relative";
-upgrade1.upgrade.style.transformOrigin = "center";
-upgrade1.upgrade.style.bottom = "50px";
-upgrade1.upgrade.style.left = "435px";
-
-upgrade2.upgrade.style.position = "relative";
-upgrade2.upgrade.style.transformOrigin = "center";
-upgrade2.upgrade.style.bottom = "0px";
-upgrade2.upgrade.style.left = "140px";
-
-upgrade3.upgrade.style.position = "relative";
-upgrade3.upgrade.style.transformOrigin = "center";
-upgrade3.upgrade.style.top = "50px";
-upgrade3.upgrade.style.right = "295px";
-
-const u1Text: string = `Investments made: ${upgrade1.timesBought}`;
-const u1Element = document.createElement("div");
-u1Element.innerHTML = u1Text;
-u1Element.style.position = "absolute";
-u1Element.style.top = "90px";
-u1Element.style.left = "60px";
-app.append(u1Element);
-
-const u2Text: string = `Posts created: ${upgrade2.timesBought}`;
-const u2Element = document.createElement("div");
-u2Element.innerHTML = u2Text;
-u2Element.style.position = "absolute";
-u2Element.style.top = "120px";
-u2Element.style.left = "60px";
-app.append(u2Element);
-
-const u3Text: string = `Funds drained: ${upgrade3.timesBought}`;
-const u3Element = document.createElement("div");
-u3Element.innerHTML = u3Text;
-u3Element.style.position = "absolute";
-u3Element.style.top = "150px";
-u3Element.style.left = "60px";
-app.append(u3Element);
 
 // Start the loop
 requestAnimationFrame(updateCounter);
